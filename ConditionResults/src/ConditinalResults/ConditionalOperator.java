4bfe085508fa536ca742090f6337f309e8d18cb1
Admin <Admin@DESKTOP-4IMVIKK>
2023-10-04 13:28:12 +0530

    My task

---
+++ ConditionResults/src/ConditinalResults/ConditionalOperator.java
@@ -0,0 +1,33 @@
+package ConditinalResults;
+
+public class ConditionalOperator {
+
+	public static void main(String[] args) {
+		
+		
+		int x = 5;
+		int y = 7;
+		boolean EqualCond = (x == y);  // Equal to condition 
+		boolean NotEqualCond = (x != y); // Not equal condition
+		boolean greaterthan = (x > y);   //Greater than
+		boolean Lessthan = (x < y);  //Less than
+		boolean grethaequal =(x >= y); //Greater than equal to
+		boolean ltre  = (x <= y);  //less than equal to
+	
+		
+		
+		
+		
+		System.out.println(EqualCond);
+		System.out.println(NotEqualCond);
+		System.out.println(greaterthan);
+		System.out.println(Lessthan);
+		System.out.println(grethaequal);
+		System.out.println(ltre);
+		
+		
+
+
+	}
+
+}
